c7fb191377459c95de349b11e232bfbdabf9e571
kukushking <3997468+kukushking@users.noreply.github.com>
2021-06-27 13:20:10 +0100

    Minor - improved error handling

--- bit_playlist_bot/lambda/handler.py
+++ bit_playlist_bot/lambda/handler.py
@@ -21,25 +21,35 @@ _playlist_id = Config.get_playlist_id()
 def lambda_handler(event: Dict, context: Dict):
     _logger.info(f"Event: {event}, context: {context}")
 
-    for url in get_urls_from_message(json.loads(event["body"])["message"]["text"]):
+    success_resp = {
+        "statusCode": 200
+    }
+
+    try:
+        message_text = json.loads(event["body"])["message"]["text"]
+    except Exception as e:
+        _logger.info(f"No message: '{e}'")
+        return success_resp
+
+    for url in get_urls_from_message(message_text):
         try:
             if is_track(url):
                 track_id: str = get_id_from_url(url)
             elif is_album(url):
                 album_id: str = get_id_from_url(url)
-                _logger.info(f"Album id: {album_id}")
                 track_id: str = _spotify.get_album_track_ids(album_id=album_id)[0]
+            else:
+                _logger.info(f"Unknown URL '{url}'")
+                return success_resp
+
             _logger.info(f"Track id: {track_id}")
 
             response: Optional[Any] = _spotify.add_to_playlist(
                 Config.get_playlist_id(),
                 [track_id],
             )
-            _logger.info(f"Response: {response}")
+            _logger.info(f"Add to playlist response: '{response}'")
         except Exception as e:
-            _logger.error(f"Error processing '{url}': '{e}'")
+            _logger.error(f"Failed to process '{url}': '{e}'")
             raise e
-
-    return {
-        "statusCode": 200,
-    }
+    return success_resp
